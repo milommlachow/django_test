3746d2087faed8ed72a8f16daea2d0956f89a511
Milon Mlachow <milonmlachow@gmail.com>
2023-02-06 16:17:18 +0300

    test4

--- git_test/test_git.py
+++ git_test/test_git.py
@@ -10,7 +10,12 @@ def moun(a, b):
     return a * b
 
 
+def neo(a, b):
+    return a // b
+
+
 if __name__ == '__main':
     print(plus(2, 2))
     print(minus(4, 2))
     print(moun(6, 2))
+    print(neo(8, 2))
