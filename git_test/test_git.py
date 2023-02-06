b48ba6a2087af4965977f88eef0b533d5ee99599
Milon Mlachow <milonmlachow@gmail.com>
2023-02-06 16:14:41 +0300

    test3

--- git_test/test_git.py
+++ git_test/test_git.py
@@ -6,6 +6,11 @@ def minus(a, b):
     return a - b
 
 
+def moun(a, b):
+    return a * b
+
+
 if __name__ == '__main':
     print(plus(2, 2))
-    print(min(4, 2))
+    print(minus(4, 2))
+    print(moun(6, 2))
